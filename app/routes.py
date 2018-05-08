266b504cd464351efaa3f76c8a67ef6da3bf8997
madman733 <alexandr.veresov@gmail.com>
2018-05-08 23:09:16 +0400

    first steps

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    message_id = {'message_id': '123'}
+    message_bodies = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Иполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', message_id=message_id, message_bodies=message_bodies)
