006b3c6e50904860137dd129438dd99cc2aed676
madman733 <alexandr.veresov@gmail.com>
2018-05-11 17:49:19 +0400

    data template

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, Label
 from wtforms.validators import DataRequired
 
 
@@ -10,4 +10,4 @@ class SomeForm(FlaskForm):
     lenght = IntegerField('lenght')
     #remember_me = BooleanField('Remember Me ')
     submit = SubmitField('Get')
-
+    msg_text = StringField('msg_text')
