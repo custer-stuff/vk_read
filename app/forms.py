91bf57fb32e36a42bb9dcef601fa85eeabe668ec
madman733 <alexandr.veresov@gmail.com>
2018-05-11 05:28:35 +0400

    refactor guide project for my project

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,9 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me ')
+
+class SomeForm(FlaskForm):
+    username = StringField('msg_id', validators=[DataRequired()])
+    password = StringField('lenght', validators=[DataRequired()])
+    #remember_me = BooleanField('Remember Me ')
     submit = SubmitField('Sign in')
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import SomeForm
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    message_id = {'message_id': '123'}
-    message_bodies = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Иполит'},
-            'body': 'Какая гадость эта ваша заливная рыба!!'
-        }
-    ]
-    return render_template('index.html', title='Home', message_id=message_id, message_bodies=message_bodies)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+    form = SomeForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Get message for message_id {}'.format(
+            form.username.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Main', form=form)
